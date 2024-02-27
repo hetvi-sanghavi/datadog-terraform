5db6e837986d00ba70b08263eb5c9505b9e129f6
hetvi-sanghavi <93520492+hetvi-sanghavi@users.noreply.github.com>
2024-02-27 13:28:59 +0530

    Update

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "datadog_monitor" "cpumonitor" {
   type = "metric alert"
   message = "CPU usage alert"
   query = "avg(last_1m):avg:system.cpu.system{*} by {host} > 60"
-  thresholds = {
+  monitor_thresholds = {
     ok = 20
     warning = 50
     critical = 60
