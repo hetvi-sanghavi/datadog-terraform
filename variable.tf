879681e51c9862226c88d761176848922cbaf7cc
hetvi-sanghavi <93520492+hetvi-sanghavi@users.noreply.github.com>
2024-02-29 22:46:29 +0530

    Synthetic tets

--- variable.tf
+++ variable.tf
@@ -1,5 +1,6 @@
-variable "monitor_name" {
-  type = string
+locals {
+  global_variable_id   = datadog_synthetics_global_variable.global_variable.id
+  global_variable_name = datadog_synthetics_global_variable.global_variable.name
 }
 variable "global_variable_name" {
   type = string
@@ -7,15 +8,11 @@ variable "global_variable_name" {
 variable "global_variable_value" {
   type = string
 }
+variable "datadog_synthetics_test_name" {
+  type = string
+  default = "inductiveuniversity.com"
+}
 variable "global_variable_description" {
   type    = string
   default = "Global Variable"
-}
-variable "dashboard_name" {
-  type    = string
-  default = "MyDashboard"
-}
-locals {
-  global_variable_id   = datadog_synthetics_global_variable.global_variable.id
-  global_variable_name = datadog_synthetics_global_variable.global_variable.name
 }
